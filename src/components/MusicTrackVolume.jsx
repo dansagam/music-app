e303fbccde8cf2ea0c44500d525e5d85188174b5
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-13 01:19:34 +0100

    finish up the UI

--- src/components/MusicTrackVolume.jsx
+++ src/components/MusicTrackVolume.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+   faVolumeDown,
+   faVolumeUp,
+   // faVolumeMute, 
+   // faMusic,
+   faList,
+   faHeart as faHeartSolid,
+   faPlus,
+} from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const MusicTrackVolume = () => {
+   const volumeHandler = (e) => {
+
+   }
+   const trackSlideHandler = () => {
+
+   }
+   const [isFavourite, setIsFavourite] = useState(false)
    return (
       <div>
-
+         <div>
+            <span><FontAwesomeIcon icon={faList} /></span>
+            <span onClick={() => setIsFavourite(!isFavourite)}>
+               <FontAwesomeIcon icon={isFavourite ? faHeartSolid : faHeart} />
+            </span>
+            <span><FontAwesomeIcon icon={faPlus} /></span>
+         </div>
+         <div>
+            <input type="range" min='0' max={100} value={0} onChange={trackSlideHandler} />
+         </div>
+         <div><span>0:00</span>  <span>0:00</span></div>
+         <div>
+            <FontAwesomeIcon icon={faVolumeDown} />
+            <input type="range" min='0' max={100} value={0} onChange={volumeHandler} />
+            <FontAwesomeIcon icon={faVolumeUp} />
+         </div>
       </div>
    )
 }
