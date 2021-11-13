959a4b659a765a4c4b1fb9f653dfb83bbd693c88
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-13 01:16:44 +0100

    creat the slice reducer

---
+++ src/reducers/trackListReducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const trackListReducer = createSlice({
+   name: 'music',
+   initialState: {
+      music_lists: [],
+      playing_music: {
+
+      }
+   },
+   reducers: {
+
+   }
+})
+
+
+export default trackListReducer.reducer
