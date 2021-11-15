2f25edb9363850e43799b5a587dd7c119bacf180
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-15 12:31:06 +0100

    modified the impor t

--- src/App.js
+++ src/App.js
@@ -1,65 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import './App.css';
-import MusicOps from './components/PlayingFeatures/MusicOps';
-import MusicPlayingDisplay from './components/PlayingFeatures/MusicPlayingDisplay';
-import MusicTrackVolume from './components/PlayingFeatures/MusicTrackVolume';
-import {
-   loadTrack,
-} from './reducers/trackListReducer';
+import MusicPlayer from './components/PlayingFeatures/MusicPlayer';
 
 function App() {
-   const {
-      // music_lists,
-      playing_music,
-      success,
-      isPlayingTrack,
-   } = useSelector(state => state.Music)
-   const dispatch = useDispatch()
-   const track_index = 0
-   const track_audio = useRef(null)
-   // 
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-   // const sliderHandler = () => {
-   //    let seekPosition = 0
-   //    if (!isNaN(track_audio.current.duration)) {
-   //       seekPosition = (track_audio.current.currentTime * (100 / track_audio.current.duration)).toFixed(2)
-   //       setSliderTrack(seekPosition)
-   //    }
-   // }
-   useEffect(() => {
-      let intervalId
-      if (playing_music.music === '') {
-         dispatch(loadTrack(track_index))
-      } else {
-         // if (
-         //    success.nextTrackSucccess
-         //    || success.prevTrackSuccess
-         //    || success.playTrackSuccess) {
-         // loadTrack(playing_music)
-         // setInterval(sliderHandler, 1000)
-         // } else {
-         // clearInterval(setUpdateInterval)
-         //    }
-      }
-      return () => clearInterval(intervalId)
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [playing_music, dispatch, track_index, track_audio, success, isPlayingTrack])
    return (
       <div className="App">
-         <main>
-            testing
-         </main>
-         <header className="App-header">
-            <audio ref={track_audio} src={playing_music.music}></audio>
-            <MusicPlayingDisplay trackDisplay={playing_music} />
-            <MusicTrackVolume
-               curr_track={track_audio}
-            />
-            <MusicOps
-               curr_track={track_audio}
-            />
-         </header>
+         <MusicPlayer />
       </div>
    );
 }
